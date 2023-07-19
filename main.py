ddc44fbe76aea2a95eaf1a5cec41c720488a3fce
Writerfrighter <noahjfang@outlook.com>
2023-07-19 11:21:30 -0700

    Worked on stuff

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, session
-
+from datetime import datetime
+import numpy as np
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+	return render_template('index.html')
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/scouting')
+def scouting():
+	return render_template('scouting.html', current_year = datetime.now().year, years = np.flip(np.arange(start=1992, stop=datetime.now().year)))
+app.run(host='0.0.0.0', port=8080, debug=True) 
